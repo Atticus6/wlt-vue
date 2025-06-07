8fbaf351c5f7ea447ef761cf5f53c16b6024b5b6
Atticus6 <94210351+Atticus6@users.noreply.github.com>
2025-06-07 21:28:17 +0800

    feat:删除多余代码

--- apps/play/src/main.ts
+++ apps/play/src/main.ts
@@ -1,8 +1,7 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 import { effect, reactive } from "@wlt/reactivity";
+
+const app = document.getElementById("app")!;
 
 const v = {
   name: "wlt",
@@ -15,29 +14,10 @@ const v = {
 const user = reactive(v);
 
 effect(() => {
+  app.innerHTML = `name:${user.name}`;
   console.log("用户执行函数", user.name, user.age);
 });
 
 setTimeout(() => {
   user.name = "atticus";
 }, 1000);
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
